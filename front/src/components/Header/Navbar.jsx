0c12db52d627a4970510707b4f73968c028b5376
Josue Gonzalez <josuegr834@gmail.com>
2024-07-10 15:11:05 -0300

    Adaptando el navbar y sus estilos a los dispositivos

--- front/src/components/Header/Navbar.jsx
+++ front/src/components/Header/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     <>
       <nav className="navbar-content">
         <div className="divisor">
-          <h1 className="nav-title">Edén de Sabores</h1>
+          <h2 className="nav-title">Edén de Sabores</h2>
           <button className="dropdown-toggle" onClick={handleClick}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
