e4d1ec429b4f186c3646229851549a45ded2cdf2
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-07-11 18:00:08 -0300

    Añadi react-router-dom (implementado) y react-icons

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+// import { AuthView } from "./components/AuthView/AuthView";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import "./App.css";
+
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        {/* <Route path="/login" element={<AuthView />} /> */}
+      </Routes>
     </>
   );
 }
