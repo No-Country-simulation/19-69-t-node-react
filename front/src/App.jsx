8b503fe83358fd1d47253b5cd7cbcfa9dbedcbd5
Juanma Garrido <juangpdev@gmail.com>
2024-07-12 19:43:15 +0200

    confirmo cambios antes de pull

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
-import "./App.css";
 
 function App() {
   return (
-    <>
+
+    <div className="flex flex-col min-h-screen">
       <Header />
       <Main />
-    </>
+      <div className="flex-grow"></div> {/* Este div actúa como un espacio flexible */}
+      <Footer />
+    </div>
+
   );
 }
 
