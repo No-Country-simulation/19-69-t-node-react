ed8663aa4d2b8c4c2138ea7fb22fe7d11eae5a95
eliteasadev <elitefirelight.dev@gmail.com>
2024-07-14 17:24:05 -0600

    ts initial configuration

--- back/src/server.ts
+++ back/src/server.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (_req, res) => {
+    res.send("Hola mundo");
+});
+
 app.listen(port, () => {
     console.log("Servidor corriendo en puerto 3000");
 });
