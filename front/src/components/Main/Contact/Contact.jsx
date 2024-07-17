acb9bed05ba6d1d44135c4d58e3555c7ab3e965f
Josue Gonzalez <josuegr834@gmail.com>
2024-07-17 20:38:09 -0300

    Importando los componentes de Formulario y Mapa al componente de Contacto

--- front/src/components/Main/Contact/Contact.jsx
+++ front/src/components/Main/Contact/Contact.jsx
@@ -1,7 +1,15 @@
+import FormContact from "./FormContact";
+import LocationMap from "./LocationMap";
+
 export default function Contact() {
   return (
-    <section>
-      <h1>Contactanos</h1>
+    <section className="bg-custom-100 text-white py-5 pt-10 px-3 md:px-5 lg:px-10">
+      <div>
+        <h1 className="uppercase">Contact</h1>
+        <p className="text-4xl font-medium">Contactanos</p>
+      </div>
+      <FormContact />
+      <LocationMap />
     </section>
   );
 }
