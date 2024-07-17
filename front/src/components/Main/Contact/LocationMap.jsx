ef5e0426236fb3b6194729197ea33d17adc3778e
Josue Gonzalez <josuegr834@gmail.com>
2024-07-17 20:36:35 -0300

    Cree el componente de Mapa para la Localizacion del restaurante

---
+++ front/src/components/Main/Contact/LocationMap.jsx
@@ -0,0 +1,13 @@
+export default function LocationMap() {
+  return (
+    <>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.027398817905!2d-58.42199988977204!3d-34.603468672841466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca622808d207%3A0x687fa5462cdacc02!2sGratto!5e0!3m2!1ses-419!2sar!4v1721255531049!5m2!1ses-419!2sar"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="w-full max-w-full mx-auto py-4 md:w-3/4"
+      ></iframe>
+    </>
+  );
+}
