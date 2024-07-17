d1f5635f4f8310e78ef1a340c859f8d165ae5a2b
Josue Gonzalez <josuegr834@gmail.com>
2024-07-17 20:40:12 -0300

    Puse el componente de Contacto en el componente principal

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,16 +4,20 @@ import { AuthView } from "./components/AuthView/AuthView";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-// import Footer from "./components/Footer/Footer";
 import "./App.css";
+import Contact from "./components/Main/Contact/Contact";
 
 function App() {
   return (
     <>
-      {window.location.pathname !== "/login" ? <Header /> : null}
+      {window.location.pathname !== "/login" &&
+      window.location.pathname !== "/contact" ? (
+        <Header />
+      ) : null}
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/login" element={<AuthView />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       {window.location.pathname !== "/login" ? <Footer /> : null}
     </>
