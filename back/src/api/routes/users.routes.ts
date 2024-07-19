f98a0215f636e36284d37762c8c5390d56a4932b
Juan Pimienta <juanmapimientae@gmail.com>
2024-07-19 14:05:35 -0500

    Creado crud de usuarios

--- back/src/api/routes/users.routes.ts
+++ back/src/api/routes/users.routes.ts
@@ -1,3 +1,50 @@
 import { Router } from "express";
+import { prisma } from "../utils/db.js";
+import bcrypt from "bcrypt"
 const router = Router();
+
+router.post("/", async (req, res) => {
+    const password =  await req.body.password;
+    const hash = bcrypt.hash(password, 10)
+    const newUser = await prisma.user.create({
+        data: req.body,
+        pasword: hash
+    })
+    res.json(newUser);
+});
+
+router.get("/", async (req, res) => {
+    const findUsers = prisma.user.findMany();
+    res.json(findUsers);
+});
+
+router.get("/:id", async (req, res) => {
+    const user = prisma.user.findUnique({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(user);
+});
+
+router.put("/:id", async (req, res) => {
+    const userToUpdate = prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: req.body
+    })
+    res.json(userToUpdate);
+});
+
+router.delete("/", async (req, res) =>{
+    const userToDelete = prisma.user.findByIdAndDelete({
+        where: {
+            id: req.body.id
+        }
+    })
+    res.json("Usuario eliminado");
+})
+
+
 export default router;
