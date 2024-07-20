df67ff84d3f623be6ddebb6505e31c39aba61f23
Josue Gonzalez <josuegr834@gmail.com>
2024-07-20 16:34:46 -0300

    Cambiando la imagen de fondo del Main a la del diseño

--- front/src/components/Main/ImageBackground.jsx
+++ front/src/components/Main/ImageBackground.jsx
@@ -2,7 +2,7 @@ export default function ImageBackground({ children }) {
   return (
     <section className="section-welcome">
       <img
-        src="/imgs/bg-image.jpg"
+        src="/imgs/bg-image.webp"
         alt="Imagen del background de Eden de Sabores"
         className="background-image"
       />
