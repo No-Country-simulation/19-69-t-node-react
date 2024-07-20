6a95701195ac0caafe18bcf1802382c294e3896d
Josue Gonzalez <josuegr834@gmail.com>
2024-07-20 18:24:37 -0300

    Modificando la direccion del logo

--- front/src/components/Header/Logo.jsx
+++ front/src/components/Header/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="logo-container">
       <img
-        src="./public/imgs/logo-principal.webp"
+        src="/imgs/logo-principal.webp"
         alt="Logo de Edén de Sabores"
         className="logo w-20 h-20 rounded-full"
       />
