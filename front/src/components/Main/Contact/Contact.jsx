4d777fea6f197eae61128972e750acf914af595a
Josue Gonzalez <josuegr834@gmail.com>
2024-07-25 02:37:59 -0300

    Arregle el componente de contacto y el mapa en moviles y tablets

--- front/src/components/Main/Contact/Contact.jsx
+++ front/src/components/Main/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import LocationMap from "./LocationMap";
 
 export default function Contact() {
   return (
-    <section className="bg-main-color text-white py-5 pt-10 px-3 md:px-5 lg:px-10 h-screen">
+    <section className="bg-main-color text-white py-16 px-4 md:px-10 md:h-screen">
       <div className="relative top-16">
         <div>
           <h1 className="uppercase">Contact</h1>
