bf9eb329ed5314236a44dfa64d6973e524d80905
Josue Gonzalez <josuegr834@gmail.com>
2024-08-01 22:28:18 -0300

    Le quite las props al componente de Colombia

--- front/src/components/Plate/PlateColombia.jsx
+++ front/src/components/Plate/PlateColombia.jsx
@@ -1,29 +1,10 @@
-export default function PlateColombia({
-  country, // nombre del pais
-  backgroundUrl, // url de la imagen de fondo
-  firstPlateUrl, // url de la primera imagen de platillo
-  firstDrinkUrl, // url de la primera imagen de bebida
-  firstPlateTitle, // titulo de la primera imagen de platillo
-  firstPlateDescription, // descripcion de la primera imagen de platillo
-  firstPlatePrice, // precio de la primera imagen de platillo
-  secondPlateUrl, // url de la segunda imagen de platillo
-  secondPlateTitle, // titulo de la segunda imagen de platillo
-  secondPlateDescription, // descripcion de la segunda imagen de platillo
-  secondPlatePrice, // precio de la segunda imagen de platillo
-  secondDrinkUrl, // url de la segunda imagen de bebida
-  firstDrinkTitle, // titulo de la primera imagen de bebida
-  secondDrinkTitle, // titulo de la segunda imagen de bebida
-  firstDrinkPrice, // precio de la primera imagen de bebida
-  secondDrinkPrice, // precio de la segunda imagen de bebida
-  firstDrinkDescription, // descripcion de la primera imagen de bebida
-  secondDrinkDescription, // descripcion de la segunda imagen de bebida
-}) {
+export default function PlateColombia() {
   return (
     <section className="relative w-full max-w-full py-32 flex flex-col items-center justify-center z-0 text-white">
       <div
         className="absolute inset-0 bg-black"
         style={{
-          backgroundImage: `url(${backgroundUrl})`,
+          backgroundImage: `url(/imgs/background-colombia.webp)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -32,76 +13,70 @@ export default function PlateColombia({
       />
       <div className="relative z-10 text-center w-full max-w-full">
         <h1 className="uppercase font-semibold text-4xl ">
-          Platos de {country}
+          Platos de Colombia
         </h1>
         <h2 className="text-3xl font-medium mt-12 uppercase">Platillos</h2>
         <div className="flex flex-col lg:flex-row flex-grow items-center justify-center gap-14 mt-8 px-4 max-w-full sm:px-14">
           <div className="flex flex-col items-center justify-center w-full md:w-[60%] lg:w-[30%] bg-black gap-6 p-7 border-white border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 bg-opacity-70 h-[505] text-pretty">
             <img
-              src={firstPlateUrl}
+              src="/imgs/colombia-arepas-con-queso.webp"
               alt="Imagen de Tacos al Pastor"
               className="h-[305] max-h-full"
             />
             <div className="flex items-center justify-between w-full">
-              <h3 className="text-2xl font-bold underline">
-                {firstPlateTitle}
-              </h3>
-              <p className="text-2xl text-yellow-600 font-medium">
-                {firstPlatePrice}
-              </p>
+              <h3 className="text-2xl font-bold underline">Arepas con Queso</h3>
+              <p className="text-2xl text-yellow-600 font-medium">$7</p>
             </div>
-            <p>{firstPlateDescription}</p>
+            <p>
+              Tortillas de maíz rellenas de queso derretido, servidas con
+              mantequilla.
+            </p>
           </div>
           <div className="flex flex-col items-center justify-center w-full md:w-[60%] lg:w-[30%] bg-black gap-6 p-7 border-white border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 bg-opacity-70 h-[505] text-pretty">
             <img
-              src={secondPlateUrl}
+              src="/imgs/colombia-bandeja-paisa.webp"
               alt="Imagen de Totopos con Guacamole"
               className="h-[305] max-h-full"
             />
             <div className="flex items-center justify-between w-full">
-              <h3 className="text-2xl font-bold underline">
-                {secondPlateTitle}
-              </h3>
-              <p className="text-2xl text-yellow-600 font-medium">
-                {secondPlatePrice}
-              </p>
+              <h3 className="text-2xl font-bold underline">Bandeja Paisa</h3>
+              <p className="text-2xl text-yellow-600 font-medium">$15</p>
             </div>
-            <p>{secondPlateDescription}</p>
+            <p>
+              Frijoles, arroz, carne molida, chicharrón, huevo frito, plátano
+              maduro, arepa y aguacate.
+            </p>
           </div>
         </div>
         <h2 className="text-3xl font-medium mt-12 uppercase">Bebidas</h2>
         <div className="flex flex-col lg:flex-row flex-grow items-center justify-center gap-14 mt-8 px-4 max-w-full md:px-14">
           <div className="flex flex-col items-center justify-center w-full md:w-[60%] lg:w-[30%] bg-black gap-6 p-7 border-white border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 bg-opacity-70 h-[505] text-pretty">
             <img
-              src={firstDrinkUrl}
+              src="/imgs/colombia-aguapanela.webp"
               alt="Imagen de Margarita"
               className="h-[305] max-h-full"
             />
             <div className="flex items-center justify-between w-full">
-              <h3 className="text-2xl font-bold underline">
-                {firstDrinkTitle}
-              </h3>
-              <p className="text-2xl text-yellow-600 font-medium">
-                {firstDrinkPrice}
-              </p>
+              <h3 className="text-2xl font-bold underline">Aguapanela</h3>
+              <p className="text-2xl text-yellow-600 font-medium">$20</p>
             </div>
-            <p>{firstDrinkDescription}</p>
+            <p>
+              Bebida refrescante hecha de panela disuelta en agua con limón.
+            </p>
           </div>
           <div className="flex flex-col items-center justify-center w-full md:w-[60%] lg:w-[30%] bg-black gap-6 p-7 border-white border-2 rounded-lg cursor-pointer transition-transform duration-300 hover:scale-105 bg-opacity-70 h-[505] text-pretty">
             <img
-              src={secondDrinkUrl}
+              src="/imgs/colombia-refajo.webp"
               alt="Imagen de agua de Jamaica"
               className="h-[305] max-h-full"
             />
             <div className="flex items-center justify-between w-full">
-              <h3 className="text-2xl font-bold underline">
-                {secondDrinkTitle}
-              </h3>
-              <p className="text-2xl text-yellow-600 font-medium">
-                {secondDrinkPrice}
-              </p>
+              <h3 className="text-2xl font-bold underline">Refajo</h3>
+              <p className="text-2xl text-yellow-600 font-medium">$22</p>
             </div>
-            <p>{secondDrinkDescription}</p>
+            <p>
+              Mezcla de cerveza y gaseosa, ideal para acompañar platos típicos.
+            </p>
           </div>
         </div>
       </div>
